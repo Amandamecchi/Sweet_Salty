44bdc3b25bd9cce68ff52d1b667728c9113f9b45
Amanda Gomes Mechi <amandamechi327@gmail.com>
2025-09-09 16:08:54 -0300

    first commit

---
+++ src/app/home/page.jsx
@@ -0,0 +1,10 @@
+import styles from "./home.module.css";
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <h1>Oi</h1>
+            <p>sei lá</p>
+        </div>
+    );
+}
